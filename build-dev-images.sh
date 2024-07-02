be3a5774a12c189863084a875e86bfdae97154d7
jiangwenbin <wenbin.jiang@coollu.com.cn>
2024-07-02 11:35:45 +0800

    feat: 文章内图片站内预览查看
    fix #20

--- build-dev-images.sh
+++ build-dev-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build . -f Dockerfile.dev --target frontend --tag fullstack-blog-frontend:latest
-docker build . -f Dockerfile.dev --target backend --tag fullstack-blog-backend:latest
+docker build . -f Dockerfile.dev --target frontend --tag fullstack-blog-frontend:dev
+docker build . -f Dockerfile.dev --target backend --tag fullstack-blog-backend:dev
 
 echo "build images succssfully."
