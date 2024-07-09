b1ab0987fd70da6c79f80887469cd3af1564854e
jiangwenbin <wenbin.jiang@coollu.com.cn>
2024-07-09 19:05:58 +0800

    fix(category): category Controller 参数校验完善
    fix #50

--- app/backend/src/controllers/category.js
+++ app/backend/src/controllers/category.js
@@ -2,15 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 const omit = require("lodash/omit");
+const { query, matchedData, body } = require("express-validator");
 const indexSQL = require("../sql");
 const dbUtils = require("../utils/db");
+const { validateInterceptor } = require("../utils/validate");
 
 /**
- * @param {Number} getCount 是否需要同时查出每个分类下的文章数量
+ * @param {Boolean} getCount 是否需要同时查出每个分类下的文章数量
  * @description 查询分类
  */
-router.get("/all", (req, res, next) => {
-    const sql = req.query.getCount ? indexSQL.QueryCategoryAndCount : indexSQL.QueryAllCategories;
+router.get("/all", [query("getCount").optional().isBoolean(), validateInterceptor], (req, res, next) => {
+    const { getCount } = matchedData(req);
+    const sql = getCount ? indexSQL.QueryCategoryAndCount : indexSQL.QueryAllCategories;
     dbUtils.query(sql).then(({ results }) => {
         if (results) {
             res.send({
@@ -48,17 +51,12 @@ router.get("/count", (req, res, next) => {
 /**
  * 模糊查询分类
  */
-router.get("/fuzzy", async (req, res) => {
-    const params = req.query;
-    if (!params.wd) {
-        return res.status(400).json({
-            msg: "请求有误",
-        });
-    }
+router.get("/fuzzy", [query("wd").notEmpty(), validateInterceptor], async (req, res) => {
+    const { wd } = matchedData(req);
     try {
         const { results } = await dbUtils.query({
             sql: indexSQL.FuzzyQueryCategory,
-            values: [`%${params.wd}%`],
+            values: [`%${wd}%`],
         });
         res.send({
             code: "0",
@@ -74,65 +72,65 @@ router.get("/fuzzy", async (req, res) => {
 /**
  * 管理员分页获取
  */
-router.get("/admin/page", (req, res, next) => {
-    const params = req.query;
-    const pageNo = Number(params.pageNo || 1);
-    const pageSize = Number(params.pageSize || 10);
-    if (Number.isNaN(pageNo) || Number.isNaN(pageSize)) {
-        return res.status(400).json({
-            msg: "请求有误",
+router.get(
+    "/admin/page",
+    [
+        query("pageNo").optional().isNumeric().toInt().default(1),
+        query("pageSize").optional().isNumeric().toInt().default(10),
+        validateInterceptor,
+    ],
+    (req, res, next) => {
+        const { pageNo, pageSize } = matchedData(req);
+        const sqlParams = [(pageNo - 1) * pageSize, pageSize];
+        dbUtils.query({ sql: indexSQL.GetCategoryAdminPage, values: sqlParams }).then(({ results }) => {
+            if (results) {
+                const list = results[0].map((item) => ({
+                    ...omit(item, "article_ids"),
+                    article_count: item.article_ids ? item.article_ids.split(",").length : 0,
+                }));
+                res.send({
+                    code: "0",
+                    data: list,
+                    total: results[1][0].total,
+                });
+            } else {
+                res.send({
+                    code: "0007003",
+                    data: [],
+                });
+            }
         });
     }
-    const sqlParams = [(pageNo - 1) * pageSize, pageSize];
-    dbUtils.query({ sql: indexSQL.GetCategoryAdminPage, values: sqlParams }).then(({ results }) => {
-        if (results) {
-            const list = results[0].map((item) => ({
-                ...omit(item, "article_ids"),
-                article_count: item.article_ids ? item.article_ids.split(",").length : 0,
-            }));
-            res.send({
-                code: "0",
-                data: list,
-                total: results[1][0].total,
-            });
-        } else {
-            res.send({
-                code: "0007003",
-                data: [],
-            });
-        }
-    });
-});
+);
 
 /**
  * 管理员编辑分类
  */
-router.put("/admin/update", (req, res, next) => {
-    const { category_name, poster, id } = req.body;
-    if (Number.isNaN(id) || typeof category_name !== "string" || typeof poster !== "string") {
-        return res.status(400).json({
-            msg: "请求有误",
+router.put(
+    "/admin/update",
+    [body("id").isInt(), body("category_name").notEmpty(), body("poster").notEmpty(), validateInterceptor],
+    (req, res, next) => {
+        const { category_name, poster, id } = matchedData(req);
+        const params = {
+            category_name,
+            poster,
+        };
+        dbUtils.query({ sql: indexSQL.AdminUpdateCategory, values: [params, id] }).then(({ results }) => {
+            if (results) {
+                // 查询成功
+                res.send({
+                    code: "0",
+                    data: null,
+                });
+            } else {
+                // 查询失败
+                res.send({
+                    code: "007004",
+                    data: 0,
+                });
+            }
         });
     }
-    const params = {
-        category_name,
-        poster,
-    };
-    dbUtils.query({ sql: indexSQL.AdminUpdateCategory, values: [params, id] }).then(({ results }) => {
-        if (results) {
-            // 查询成功
-            res.send({
-                code: "0",
-                data: null,
-            });
-        } else {
-            // 查询失败
-            res.send({
-                code: "007004",
-                data: 0,
-            });
-        }
-    });
-});
+);
 
 module.exports = router;
