6c52dc211126826d20a6062a6fd0c5d585c5740a
cumt-robin <1148121254@qq.com>
2025-03-10 14:54:08 +0800

    chore(nuxt3-web): 引入 eslint, stylelint, prettier
    fix #219

---
+++ app/nuxt3-web/eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tsEslint from "typescript-eslint";
+import eslintPluginVue from "eslint-plugin-vue";
+import vueParser from "vue-eslint-parser";
+
+export default tsEslint.config(
+    { ignores: ["node_modules", ".nuxt", ".output", "*.d.ts"] },
+    {
+        extends: [pluginJs.configs.recommended, ...tsEslint.configs.recommended, ...eslintPluginVue.configs["flat/recommended"]],
+        files: ["**/*.{js,ts,tsx,vue}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: globals.browser,
+            parser: vueParser,
+            parserOptions: {
+                parser: tsEslint.parser,
+            },
+        },
+        rules: {
+            "prettier/prettier": "error",
+            "no-unused-vars": "warn",
+        },
+    },
+    eslintPluginPrettierRecommended,
+    eslintConfigPrettier,
+);
