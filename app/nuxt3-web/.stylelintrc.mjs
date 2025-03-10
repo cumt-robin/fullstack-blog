e63ace7af550f6bfe394a348930ccf5f1a8c2126
cumt-robin <1148121254@qq.com>
2025-03-10 14:54:08 +0800

    chore(nuxt3-web): 引入 eslint, stylelint, prettier
    fix #219

---
+++ app/nuxt3-web/.stylelintrc.mjs
@@ -0,0 +1,4 @@
+/** @type {import('stylelint').Config} */
+export default {
+    extends: ["stylelint-config-standard", "stylelint-config-standard-scss", "stylelint-config-recommended-vue"],
+};
--- app/nuxt3-web/app.vue
+++ app/nuxt3-web/app.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div>
+        <NuxtRouteAnnouncer />
+        <NuxtWelcome />
+    </div>
 </template>
+
+<style lang="less" scoped>
+.a {
+    position: relative;
+    width: 100px;
+    height: 100px;
+    background-color: red;
+}
+</style>
